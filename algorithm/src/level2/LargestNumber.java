8592bdd5820ef8c90de80fdfdd7e1c7652e98c49
yunamom <hellokin10@gmail.com>
2022-02-22 17:09:23 +0900

    [Level 2] 가장큰수

---
+++ algorithm/src/level2/LargestNumber.java
@@ -0,0 +1,37 @@
+package level2;
+
+import java.util.Arrays;
+
+class Solution{
+	
+	public String solution(int[] numbers) {
+		
+		// 숫자를 문자열로 변환
+		String[] result = new String[numbers.length];
+		for (int i = 0; i < numbers.length; i++) {
+			result[i] = String.valueOf(numbers[i]);
+		}
+		// 정렬
+		Arrays.sort(result, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+		
+		// 0만 여러개 있는 배열의 경우 하나의 0만 리턴
+		if (result[0].equals("0")) {
+			return "0";
+		}
+		
+		String answer = "";
+		// 정렬된 문자 하나로 합치기
+		for (String a : result) {
+			answer += a;
+		}
+		return answer;
+	}	
+}
+public class LargestNumber {
+	public static void main(String[]args) {
+		Solution test = new Solution();
+		int[] numbers = {3, 30, 34, 5, 9};
+		
+		System.out.println(test.solution(numbers));
+	}
+}
