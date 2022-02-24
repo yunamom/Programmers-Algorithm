6d6d629aa8b5a66c42d686b1907bd0924f3da382
yunamom <hellokin10@gmail.com>
2022-02-24 23:54:49 +0900

    [Level 1] 소수찾기 - 에라토스테네스의 체

---
+++ algorithm/src/level1/PrimeNumber.java
@@ -0,0 +1,35 @@
+package level1;
+
+class Solution {
+    public int solution(int n) {
+    	 int answer = 0;
+         
+         int[] number = new int[n+1];
+         //소수찾기 (에라토스테네스의 체) 배열에 2~n까지 저장
+         for(int i=2; i<=n; i++) {
+             number[i] = i;
+         }
+        
+         for(int i=2; i<=n; i++) {
+             if(number[i]==0) continue;
+             //2~n까지 지우지 않은 값은 소수
+             for(int j= 2*i; j<=n; j += i) {
+                 number[j] = 0;
+             }
+         } 
+         for(int i=0; i<number.length; i++) {
+             if(number[i]!=0) {
+                 answer++;
+             }
+         } 
+       return answer;
+   }
+}
+public class PrimeNumber {
+	
+	public static void main(String[] args) {
+		Solution test = new Solution();
+		
+		System.out.println(test.solution(50000));
+	}
+}
