a6c9b70c03468870a35c651a81a735c613afefe6
yunamom <hellokin10@gmail.com>
2022-02-28 10:45:21 +0900

    [Skill Check Level 1] 소수만들기

---
+++ algorithm/src/skill/check/level1/소수만들기.java
@@ -0,0 +1,43 @@
+package skill.check.level1;
+
+class Solution {
+    
+    public boolean isPrime(int num){
+        if( num == 1)
+            return false;
+        
+        for(int i = 2; i < num; i++){
+            if( num % i == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for(int i = 0; i < nums.length-2; i++){
+            for(int j = i + 1; j < nums.length-1; j++){
+                for(int k = j + 1; k < nums.length; k++){
+                    int r = nums[i] + nums[j] + nums[k];
+                    if( this.isPrime(r) == true ){
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+
+public class 소수만들기 {
+	
+	public static void main(String[] args) {
+		Solution test = new Solution();
+		
+		int[] nums= {1,2,7,6,4};
+		
+		System.out.println(test.solution(nums));
+	}
+}
