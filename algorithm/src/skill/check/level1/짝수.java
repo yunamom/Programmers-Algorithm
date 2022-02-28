bf707f6f910e59a4501029054690948fdf79a423
yunamom <hellokin10@gmail.com>
2022-02-28 10:46:39 +0900

    [Skill Check Level 1] 콜라츠추측

---
+++ algorithm/src/skill/check/level1/짝수.java
@@ -0,0 +1,32 @@
+package skill.check.level1;
+
+class Solution {
+    public int solution(int num) {
+       
+        int answer = 0;
+        long nums = (long)num;
+        //중요 !! long 타입으로 변환시켜서 숫자가 커질경우를 대비한다.
+        while(nums > 1){
+            if(answer >= 500){
+                answer = -1; break;
+            }
+            if(nums % 2 == 0){
+                nums/=2;
+            }else{
+                nums*=3; nums++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
+
+public class 짝수 {
+
+	public static void main(String[] args) {
+		
+		Solution test = new Solution();
+		
+		System.out.println(test.solution(626331));
+	}
+}
