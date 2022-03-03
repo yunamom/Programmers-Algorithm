d6145380e10283c09a73b831c8d8e2bbd0b33ccc
yunamom <hellokin10@gmail.com>
2022-03-03 17:08:14 +0900

    [Level 1] 행렬의덧셈

---
+++ algorithm/src/level1/행렬의덧셈.java
@@ -0,0 +1,27 @@
+package level1;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {}; answer = arr1;
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr1[i].length; j++){
+                answer[i][j] = arr1[i][j]+arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
+public class 행렬의덧셈 {
+	
+	public static void main(String[] args) {
+		
+		Solution test = new Solution();
+		// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	    int[][] arr1 = {{1,2},{2,3}};
+	    int[][] arr2 = {{3,4},{5,6}};
+	      
+		System.out.println(Arrays.deepToString(test.solution(arr1, arr2)));    
+	}
+}
